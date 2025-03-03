ce3d2e71d3b880dd706d77550663f922bae6e739
Suvrajeet Banerjee <48400198+suvrajeet01@users.noreply.github.com>
2025-03-03 12:36:09 +0000

    Update build with correct base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/upcse/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
